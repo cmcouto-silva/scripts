36715b521e3404ba9e48d2a9a11a7434140fe269
cmcouto-silva <cmcouto.silva@gmail.com>
2019-01-29 19:59:53 -0200

    update shapeit.check.R

--- shapeit-check.R
+++ shapeit-check.R
@@ -1,54 +1,73 @@
 ####################################################################################################
-##  Script Purpose: Run Shapeit in check mode, performing all required tasks to run Shapeit 
+##  Script Purpose: Run Shapeit in check mode, performing all required tasks to run Shapeit
 ##  officially
-##  
+##
 ##  Author: M.Sc. Cainã Max Couto-Silva
 ##  Email: cmcouto.silva@gmail.com / cmcouto.silva@usp.br
 ##  Institution: University of Sâo Paulo, Brazil
-##  
+##
 ##  Date: Jan 09, 2018
-##  Last modified: Jan 10, 2018
+##  Last modified: Jan 23, 2019
 ####################################################################################################
 
 #### Section: Argument Parser Options
 ####################################################################################################
 
-suppressPackageStartupMessages(if(!require(argparse)) {install.packages("argparse") })
+suppressPackageStartupMessages(if(!require(argparse)) { install.packages("argparse") })
 suppressPackageStartupMessages(library(argparse))
 
 parser <- ArgumentParser()
 
 parser$add_argument("-d", "--dataset", type="character", metavar="",
-                    help="Path to dataset and its name (without extensions)")
+                    help="[REQUIRED] Path to raw dataset (without extensions)")
 
 parser$add_argument("-r","--ref", type="character", metavar="",
-                    help="Path to genome reference panel")
+                    help="[REQUIRED] Path to map and genome reference panel")
 
 parser$add_argument("-o","--out", type="character", metavar="",
-                    help="Identification to output files (without extentions)")
+                    help="[REQUIRED] Name for output files (without extentions)")
 
-parser$add_argument( "--plink-maf", default= 0.05, type="double", metavar="",
-                     help="Numeric value for plink MAF parameter [default %(default)s]")
+parser$add_argument("--chrs", type="character", metavar="", default = "1:22",
+                    help="Chromosomes IDs. It accepts IDs comma-delimited (e.g. 1,3,5,10) or colon (e.g. 1:10) as sequence delimiter. Default set from chromosome 1 to 22.")
 
-parser$add_argument( "--plink-mind", default= 0.1, type="double", metavar="",
-                     help="Numeric value for plink mind parameter [default %(default)s]")
+parser$add_argument( "--plink-maf", type="double", metavar="",
+                     help="Numeric value for plink MAF parameter")
 
-parser$add_argument( "--plink-geno", default= 0.1, type="double", metavar="",
-                     help="Numeric value for plink geno parameter [default %(default)s]")
+parser$add_argument( "--plink-mind", type="double", metavar="",
+                     help="Numeric value for plink mind parameter")
+
+parser$add_argument( "--plink-geno", type="double", metavar="",
+                     help="Numeric value for plink geno parameter")
 
 args <- parser$parse_args()
 
-
-# Setting arguments to new variables
+## Setting arguments to new variables
 
 # Obrigatory
 dataset_id <- args$out
 raw_dataset_path <- args$dataset
 ref_path <- args$ref
+chrs <- args$chrs
+
 # Optional
-plink_maf <- args$plink_maf
-plink_mind <- args$plink_mind
-plink_geno <- args$plink_geno
+if(!is.null(args$plink_maf)) {plink_maf <- args$plink_maf}
+if(!is.null(args$plink_mind)) {plink_mind <- args$plink_mind}
+if(!is.null(args$plink_geno)) {plink_geno <- args$plink_geno}
+
+if(!is.null(chrs)) {
+  
+  if(grepl(",", chrs)) {
+    chrs <- as.numeric(unlist(strsplit(chrs, ",")))
+  } else {
+    if(grepl(":", chrs)) {
+      idx <- unlist( strsplit(chrs, ":") )
+      chrs <- seq(idx[1], idx[2])  
+    } else {
+      chrs <- as.numeric(chrs)
+    }
+  }
+  
+}
 
 #### Section: Setting & Parameters
 ####################################################################################################
@@ -56,13 +75,13 @@ plink_geno <- args$plink_geno
 # Creating directories
 cat("\n## Creating requested directories\n")
 
-if (!dir.exists(dataset_id)){ dir.create(dataset_id, showWarnings = F) 
+if (!dir.exists(dataset_id)){ dir.create(dataset_id, showWarnings = F)
   cat(paste0(" Directory '", dataset_id, "' successfully created\n"))
 } else {
   message(paste0("\n Directory", " \"", dataset_id, "\" already exists (it will be used)"))
 }
 
-directories= c('plink_filter', 'plink_split', 'shapeit_check01', 'shapeit_check02')
+directories <- c('plink_filter', 'plink_split', 'shapeit_check')
 invisible(sapply(directories, function(mkdir){
   if (!dir.exists(paste0("./",dataset_id,"/", mkdir))){ dir.create(paste0("./",dataset_id,"/", mkdir), showWarnings = F)
     cat(paste0(" Directory \"", mkdir, "\" successfully created at ", dataset_id, "\n"))
@@ -77,43 +96,46 @@ cat("\n")
 dataset_id_path <- paste0('./',dataset_id,'/')
 plink_filter_path <- paste0(dataset_id_path, 'plink_filter/')
 plink_split_path <- paste0(dataset_id_path, 'plink_split/')
-shapeit_check01_path <- paste0(dataset_id_path, 'shapeit_check01/')
-shapeit_check02_path <- paste0(dataset_id_path, 'shapeit_check02/')
+shapeit_check_path <- paste0(dataset_id_path, 'shapeit_check/')
 
 # Plink filter
+plink_v <- gsub(".*v(\\S..).*", "\\1", system("plink --version", T))
+plink <- ifelse(plink_v >= 1.9, "plink", "plink --noweb")
+
 message(paste0("\n", "## Running plink filter parameters\n"))
-system(paste("plink ", paste0("--bfile ",raw_dataset_path)," --make-bed --maf", 
-             plink_maf," --mind ", plink_mind," --geno", plink_geno," --out ", 
-             paste0(plink_filter_path, dataset_id)))
+system(paste(plink, paste0("--bfile ",raw_dataset_path),"--make-bed", 
+             if(exists("plink_maf")){paste("--maf", plink_maf)},
+             if(exists("plink_mind")){paste("--mind", plink_mind)},
+             if(exists("plink_geno")){paste("--geno", plink_geno)},
+             "--out ", paste0(plink_filter_path, dataset_id)))
 
 # Splitting chromosomes using Plink
 message(paste0("\n", "## Splitting chromosomes\n"))
 split_raw_dataset_path <- paste0(plink_filter_path, dataset_id)
 split_output <- paste0(plink_split_path, dataset_id, '_chr')
 
-for (i in 1:22) {
-  system(paste0("plink --bfile ", split_raw_dataset_path, " --chr ", i, " --make-bed --out ", split_output,i ))
+for (i in chrs) {
+  system(paste0(plink, " --bfile ", split_raw_dataset_path, " --chr ", i, " --make-bed --out ", split_output,i ))
 }
 
 # Running Shapeit in checking mode
 
 # Writting shell command for Shapeit as character vector
-shapeit_check01 <- paste0("for i in `seq 1 22`; do shapeit -check \\
+shapeit_check <- paste0("for i in $(echo ", paste(chrs, collapse = " "), "); do shapeit -check \\
                           -B ", split_output,"$i \\
                           -M ",ref_path,"/genetic_map_chr$i\\_combined_b37.txt \\
-                          --input-ref ",ref_path,"/1000GP_Phase3_chr$i.hap.gz ",ref_path,"/1000GP_Phase3_chr$i.legend.gz ",ref_path,"/1000GP_Phase3.sample \\
-                          --output-log ", shapeit_check01_path, dataset_id, '_chr',"$i.aligments; done")
+                          --output-log ", shapeit_check_path, dataset_id, '_chr',"$i; done")
 
 # Running Shapeit first check within terminal
 message(paste0("\n", "## Running Shapeit's first check"))
-system(shapeit_check01)
+system(shapeit_check)
 
-directories= c("strand","exclude","logs")
+directories <- c("snp.mm","ind.mm","logs")
 invisible(sapply(directories, function(mkdir){
-  if (!dir.exists(paste0(shapeit_check01_path, mkdir))){ 
-    dir.create(paste0(shapeit_check01_path, mkdir), showWarnings = F)
-    cat(paste0("Directory \"", mkdir, "\" successfully created at ", shapeit_check01_path, "\n"))
-  } else { 
+  if (!dir.exists(paste0(shapeit_check_path, mkdir))){
+    dir.create(paste0(shapeit_check_path, mkdir), showWarnings = F)
+    cat(paste0("Directory \"", mkdir, "\" successfully created at ", shapeit_check_path, "\n"))
+  } else {
     message(paste0(" Directory \"", mkdir, "\" already exists (it will be used)"))
   }
 }))
@@ -121,78 +143,10 @@ invisible(sapply(directories, function(mkdir){
 rm(directories)
 cat("\n")
 
-check01_strand_path <- paste0(shapeit_check01_path,'strand/')
-check01_exclude_path <- paste0(shapeit_check01_path,'exclude/')
-check01_logs_path <- paste0(shapeit_check01_path,'logs/')
+check_snp.mm_path <- paste0(shapeit_check_path,'snp.mm/')
+check_ind.mm_path <- paste0(shapeit_check_path,'ind.mm/')
+check_logs_path <- paste0(shapeit_check_path,'logs/')
 
-system(paste0("mv ", shapeit_check01_path,"*.strand ", check01_strand_path))
-system(paste0("mv ", shapeit_check01_path,"*.exclude ", check01_exclude_path))
-system(paste0("mv ", shapeit_check01_path,"*.log ", check01_logs_path))
-
-# Inverting strands using Plink
-
-message("\n## Strand inversion using plink")
-shapeit_strand_chr <- paste0(check01_strand_path,dataset_id, '_chr')
-strand_ext <- ".aligments.snp.strand"
-
-for (i in 1:22) {
-  file_name <- paste0(shapeit_strand_chr,i,strand_ext)
-  data <- read.table(file_name, header = T, row.names = NULL, stringsAsFactors = F)
-  rsID <- data[data$type=="Strand", 4]
-  RefSnps_to_flip <- unique(rsID)
-  write.table(RefSnps_to_flip, paste0(shapeit_strand_chr,i,"_strands_to_flip.txt"), 
-              sep="\t", col.names=F, row.names=F, quote=F)
-}
-
-if (!dir.exists(paste0(dataset_id_path,"shapeit_plink_flipped_strands"))) { 
-  dir.create(paste0(dataset_id_path,"shapeit_plink_flipped_strands"), showWarnings = F)
-  cat(paste0(" Directory \"", 'shapeit_plink_flipped_strands', "\" successfully created at ", dataset_id_path, "\n"))
-} else {
-  message(paste0(" Directory \"", 'shapeit_plink_flipped_strands', "' already exists (it will be used)"))
-}
-
-cat("\n")
-shapeit_plink_flipped_strands_path <- paste0(dataset_id_path,'shapeit_plink_flipped_strands/')
-
-chrs_raw_dataset_path <- paste0(plink_split_path, dataset_id,'_chr')
-chrs_output <- paste0(shapeit_plink_flipped_strands_path, dataset_id,'_chr')
-chrs_flip.ref <- shapeit_strand_chr
-
-for (i in 1:22) {
-  system(paste0("plink --bfile ", chrs_raw_dataset_path,i, " --flip ", chrs_flip.ref,i,'_strands_to_flip.txt --make-bed --out ', chrs_output,i))
-}
-
-# Running Shapeit in checking mode
-
-# Writing shell commands in R character vector
-shapeit_check02 <- paste0("for i in `seq 1 22`; do shapeit -check \\
--B ", chrs_output,"$i \\
--M ",ref_path,"/genetic_map_chr$i\\_combined_b37.txt \\
---input-ref ", ref_path,"/1000GP_Phase3_chr$i.hap.gz ", ref_path,"/1000GP_Phase3_chr$i.legend.gz ", ref_path,"/1000GP_Phase3.sample \\
---output-log ", shapeit_check02_path, dataset_id, '_chr',"$i.aligments; done")
-
-# Running Shapeit second check within terminal
-message(paste0("\n", "## Running Shapeit's second check"))
-system(shapeit_check02)
-
-directories <- c("strand","exclude","logs")
-invisible(sapply(directories, function(mkdir){
-  if (!dir.exists(paste0(shapeit_check02_path, mkdir))){ 
-    dir.create(paste0(shapeit_check02_path, mkdir), showWarnings = F) 
-    cat(paste0(" Directory \"", mkdir, "\" successfully created at ", shapeit_check02_path, "\n"))
-  } else { 
-    message(paste0(" Directory \"", mkdir, "' already exists (it will be used)"))
-  }
-}))
-
-rm(directories)
-cat("\n")
-
-check02_strand_path= paste0(shapeit_check02_path,'strand/')
-check02_exclude_path= paste0(shapeit_check02_path,'exclude/')
-check02_logs_path= paste0(shapeit_check02_path,'logs/')
-
-system(paste0("mv ", shapeit_check02_path,"*.strand ", check02_strand_path))
-system(paste0("mv ", shapeit_check02_path,"*.exclude ", check02_exclude_path))
-system(paste0("mv ", shapeit_check02_path,"*.log ", check02_logs_path))
-
+system(paste0("mv ", shapeit_check_path,"*.snp.mm ", check_snp.mm_path))
+system(paste0("mv ", shapeit_check_path,"*.ind.mm ", check_ind.mm_path))
+system(paste0("mv ", shapeit_check_path,"*.log ", check_logs_path))
